78ef46c7ba1aba04c125dbad3a280ee732a5253e
Daniele Paletti <danielepaletti98@gmail.com>
2021-01-07 13:51:37 +0100

    README update

---
+++ src/dovado_rtl/visualize.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from pathlib import Path
+import typer
+import pickle
+from pymoo.visualization.pcp import PCP
+
+
+def visualize(
+    file_path: Path = typer.Option(  # pylint: disable=unused-argument
+        ...,
+        exists=True,
+        file_okay=True,
+        readable=True,
+        resolve_path=True,
+        help=("path of the file to visualize"),
+    ),
+    graph: Optional[str] = typer.Option(
+        None, help="input which graph you would like to see"
+    ),
+):
+    if file_path.suffix == ".pkl":
+        f = open(file_path, "rb")
+        try:
+            res, algorithm = pickle.load(f)
+
+        except Exception:
+            raise Exception(
+                "Could not unpack two values from pickled value, ensure pickle file is produced by dovado"
+            )
+
+    else:
+        raise Exception("Unsupported file extension: " + str(file_path.stem))
+
+
+def run_visualize():
+    typer.run(visualize)
