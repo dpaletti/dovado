e5ed3c445a6ad887fb8205cf7d592adfd08fb5cf
dpaletti <danielepaletti98@gmail.com>
2022-04-22 17:19:22 +0200

    ScalaParsing no edge cases

--- src/dovado_rtl/src_parsing.py
+++ src/dovado_rtl/src_parsing.py
@@ -34,16 +34,6 @@ from dovado_rtl.antlr.generated.Scala.ScalaParser import ScalaParser
 from dovado_rtl.antlr.generated.Scala.ScalaLexer import ScalaLexer
 from dovado_rtl.antlr.scala_decl_visitor import ScalaDeclVisitor
 
-# TODO: remove this after Scala Visitor implementation
-def test():
-    input_stream = FileStream("example.scala")
-    lexer = ScalaLexer(input_stream)
-    token_stream = CommonTokenStream(lexer)
-    parser = ScalaParser(token_stream)
-    visitor = ScalaDeclVisitor("example.scala", token_stream)
-    tree = parser.compilationUnit()
-    visitor.visit(tree)
-    print(visitor.scala_file)
 
 
 class SourceParser(AbstractSourceParser):
