bee0c919f7989b349eb2595777ab48f72ffd4ddc
dpaletti <danielepaletti98@gmail.com>
2022-04-25 19:32:58 +0200

    Half-Working Parsing and Parameter Substitution

--- src/dovado_rtl/chisel_handling.py
+++ src/dovado_rtl/chisel_handling.py
@@ -1,12 +1,27 @@
 import json
 from pathlib import Path
 
+import pexpect
 from antlr4 import FileStream, CommonTokenStream
 
 from dovado_rtl.antlr.generated.Scala.ScalaLexer import ScalaLexer
 from dovado_rtl.antlr.generated.Scala.ScalaParser import ScalaParser
 from dovado_rtl.antlr.scala_decl_visitor import ScalaDeclVisitor
 from dovado_rtl.antlr.scala_representation import ScalaParameter
+
+
+def source_compilation_script(script_path: str):
+    bash = pexpect.spawnu(
+        "bash",
+        encoding="utf-8",
+        codec_errors="ignore",
+    )
+    bash.sendline("source " + script_path)
+    bash.expect("%Compilation End%", timeout=None)
+
+    print("\n-----------------COMPILATION OUTPUT------------------")
+    print(bash.before)
+    print("-----------------------------------------------------")
 
 
 def set_parameters(json_path: str):
