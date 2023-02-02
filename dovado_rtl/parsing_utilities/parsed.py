66c7bd10bda914222ebcd2b81aedaf97a984741e
dpaletti <dpaletti@gmail.com>
2023-02-02 12:21:56 +0100

    Scala parsing and replacing

--- dovado_rtl/parsing_utilities/parsed.py
+++ dovado_rtl/parsing_utilities/parsed.py
@@ -1,9 +1,5 @@
 from typing import Protocol
 
-FILE_ROW = int
-FILE_COLUMN = int
-POSITION = tuple[FILE_ROW, FILE_COLUMN]
-
 
 class Parsed(Protocol):
     def replace(self, parameter_to_value: dict[str, str]) -> None:
