462ed4f1e8d4237d4e336377d7c33edb14bbfd47
dpaletti <danielepaletti98@gmail.com>
2023-04-13 12:42:35 +0200

    Working flows

--- tests/resources/make_rocket.bash
+++ tests/resources/make_rocket.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-#
+
 make verilog CONFIG=freechips.rocketchip.system.DefaultFPGAConfig -C ../resources/rocket-chip/vsim
 mkdir chisel_generated
 mv ../resources/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultFPGAConfig/SimDTM.v ../resources/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultFPGAConfig/SimDTM.sv
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,5 @@ from dovado_rtl.main import main
 
 
 def test_main():
-    with patch("sys.argv", ["main.py", "resources/configs/test_config.toml"]):
+    with patch("sys.argv", ["main.py", "resources/configs/test_range_config.toml"]):
         main()
